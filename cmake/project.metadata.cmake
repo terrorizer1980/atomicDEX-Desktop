fe67b93692d7fa5b493e7669288ce151215b2a63
milerius <rmscastle@gmail.com>
2021-06-28 09:17:23 +0200

    fix(metadata): update links for smartdex

--- cmake/project.metadata.cmake
+++ cmake/project.metadata.cmake
@@ -2,10 +2,10 @@
 set(DEX_PROJECT_NAME "smartdex-desktop")
 set(DEX_DISPLAY_NAME "SmartDEX")
 set(DEX_COMPANY "SmartFi")
-set(DEX_WEBSITE "https://smartportal.smartfi.com")
+set(DEX_WEBSITE "https://smartfi.com/")
 set(DEX_MAINTENANCE_TOOL_NAME "AtomicDEX Maintenance Tool")
-set(DEX_SUPPORT_PAGE "https://support.komodoplatform.com/support/home")
-set(DEX_DISCORD "https://komodoplatform.com/discord")
+set(DEX_SUPPORT_PAGE "https://help.smartfi.com/hc/en-us")
+set(DEX_DISCORD "https://discord.gg/yrYDtEnsfg")
 set(DEX_TWITTER "https://twitter.com/smartfiportal")
 set(DEX_PRIMARY_COIN "SFUSD") ## Main coin of the DEX, will enable it by default and will be the default left ticker for trading
 set(DEX_SECOND_PRIMARY_COIN "LTC")  ## Second main coin of the DEX, will enable it by default and will be the default right ticker for trading
